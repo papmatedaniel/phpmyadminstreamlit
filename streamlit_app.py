de2576da616817d838c03f645a114dd5f6761505
papmatedaniel <90318564+papmatedaniel@users.noreply.github.com>
2024-01-18 07:54:12 +0000

    elso

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,17 @@
 import streamlit as st
-conn = st.experimental_connection('mysql', type='sql')
+
+# Specify the path to the secrets.toml file
+secrets_path = "/workspaces/phpmyadminstreamlit/.streamlit/secrets.toml"
+
+# Load database configuration from secrets.toml
+mysql_config = st.secrets["mysql"]
+
+# Establish the connection
+conn = st.experimental_connection('mysql', config=mysql_config)
+
+# Query the database
 df = conn.query('SELECT * from mytable;', ttl=600)
+
+# Display the results
 for row in df.itertuples():
     st.write(f"{row.name} has a :{row.pet}:")
